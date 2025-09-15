c478e274c0494cfa1150464a154e0983a1841469
finnik <niklas@finnik.de>
2025-09-15 15:48:37 +0200

    Display departures over 1h

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -3,6 +3,9 @@ export function formatTimeDiff(from: Date, to: Date): string {
     const totalSeconds = Math.max(Math.floor(diffMs / 1000), 0); // prevent negative times
     const minutes = Math.floor(totalSeconds / 60);
     const seconds = Math.floor((totalSeconds % 60) / 10) * 10; // round down to nearest 10
+    if (minutes>59) {
+      return "> 1h";
+    }
     return `${minutes}:${seconds.toString().padStart(2, '0')}`;
   }
   
